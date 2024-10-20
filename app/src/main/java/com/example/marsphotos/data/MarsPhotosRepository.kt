bb836dd3acd8a74dabf94a979d24ce9a61b6057a
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-20 10:58:07 +0200

    we don't need this object

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,7 +1,6 @@
 package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
-import com.example.marsphotos.network.MarsApi
 import com.example.marsphotos.network.MarsApiService
 
 interface MarsPhotosRepository {
